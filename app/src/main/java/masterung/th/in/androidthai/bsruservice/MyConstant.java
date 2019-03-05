5a743147478ca563f013450bfc660fe85519bb97
ราเมศร์ อ่างคําหงษ์ <ramet.top@gmail.com>
2019-03-05 11:49:31 +0700

    Update 12.00

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,15 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/bsru/addUserMaster.php";
+    private String urlGetUserWhere = "https://www.androidthai.in.th//bsru/getUserWhereUserTopRamet.php";
+
+    public String getUrlGetUserWhere() {
+        return urlGetUserWhere;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
